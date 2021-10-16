ff3f2d646472928998d7292478e38e3552e340b5
John Chishugi <jkchishugi@gmail.com>
2021-10-16 21:17:21 +0100

    ft-filter-month-week-and-agenda

--- src/MyCalendar.js
+++ src/MyCalendar.js
@@ -1,20 +1,29 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
 import moment from 'moment'
 import myEventsList from './events'
 
-const localizer = momentLocalizer(moment)
+const MyCalendar = () => {
+  const localizer = momentLocalizer(moment);
+  const allViews = Object.keys(Views).map(k => Views[k]);
 
-const MyCalendar = () => (
-  <div>
-    <div className="title">Calender Event</div>
-    <Calendar
-      localizer={localizer}
-      events={myEventsList}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 500 }}
-    />
-  </div>
-)
+  //Filter only month, week and Agenda view
+  const filterViews = allViews.filter((view)=>{
+      return view === 'month' ||   view === 'week' || view === 'agenda';
+  });
+
+  return (
+    <div>
+      <div className="title">Calender Event</div>
+      <Calendar
+        views={filterViews}
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  )
+}
 
 export default MyCalendar;
