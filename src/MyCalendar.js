4d609cf0533003959288aba87ebe6ffa1a1d5dba
John Chishugi <jkchishugi@gmail.com>
2021-10-16 23:34:24 +0100

    ft-week-show-only-date

--- src/MyCalendar.js
+++ src/MyCalendar.js
@@ -1,26 +1,23 @@
-import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 import myEventsList from './events'
 
 const MyCalendar = () => {
   const localizer = momentLocalizer(moment);
-  const allViews = Object.keys(Views).map(k => Views[k]);
-
-  //Filter only month, week and Agenda view
-  const filterViews = allViews.filter((view)=>{
-      return view === 'month' ||   view === 'week' || view === 'agenda';
-  });
-
+  const views = ['month', 'week', 'agenda'];
+  
   return (
     <div>
       <div className="title">Calender Event</div>
       <Calendar
-        views={filterViews}
+        views={views}
         localizer={localizer}
         events={myEventsList}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
+        onSelectEvent={event => alert(event.title)}
+
       />
     </div>
   )
