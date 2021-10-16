59a67d3e42a80ab508fbb7ff2637bbd7e819d475
John Chishugi <jkchishugi@gmail.com>
2021-10-16 20:45:19 +0100

    ft-add-div-in-App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import MyCalendar from './MyCalendar';
+
 const App = () => {
   return (
     <div className="App">
-      
+        <div className="content">
+             <MyCalendar />
+        </div>
     </div>
   );
 }
